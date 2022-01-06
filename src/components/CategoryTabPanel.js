34f490aae005cfadaf7fc71ac87a43f438a6cabf
svohra02 <66923911+svohra02@users.noreply.github.com>
2022-01-06 17:06:18 -0500

    home progress

---
+++ src/components/CategoryTabPanel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryTabPanel = ({categoryName}) => {
+    return (
+        <div>
+            <h1>{categoryName}</h1>
+        </div>
+    )
+}
+
+export default CategoryTabPanel
